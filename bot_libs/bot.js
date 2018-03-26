1000083cfa311135fc5bf25d9c61a44f98625cf1
Noah Leigh <noah.d.leigh@gmail.com>
2018-03-26 18:57:51 -0400

    Moved twitter bot handling to /bot_libs/bot.js

---
+++ bot_libs/bot.js
@@ -0,0 +1,31 @@
+const {generateRandomGif} = require('./puppeteer')
+const {getTwitterClient, sendTweet} = require('./twitter')
+
+const twitterBotHandlerGenerator = (config) => {
+  const twitterBotHandler = async (request, response) => {
+    const port = process.env.PORT
+    const scheme = `http${port === 443 ? 's' : ''}`
+    const host = `${request.hostname}:${port}`
+    const puppeteerURL = `${scheme}://${host}/puppeteer`
+    const gifResult = await generateRandomGif(
+      config, puppeteerURL,
+      '#textarea-config-string',
+      '#form-gif-config input[type="submit"]',
+      '#gif-link'
+    )
+    const client = getTwitterClient()
+    try {
+      const sentTweet = await sendTweet(client, gifResult.options.text, gifResult.imageData)
+      console.log(sentTweet)
+      response.status(200).type(gifResult.imageData.mime).send(gifResult.imageData.buffer)
+    } catch (err) {
+      console.error(err)
+      response.status(500).send(err.message)
+    }
+  }
+  return twitterBotHandler
+}
+
+module.exports = {
+  twitterBotHandlerGenerator
+}
--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 /* eslint-env node */
 
 // init project
-const process = require('process')
 const path = require('path')
+const fs = require('fs')
 const express = require('express')
 const helmet = require('helmet')
 const app = express()
-const {generateRandomGif} = require('./bot_libs/puppeteer')
-const {getTwitterClient, sendTweet} = require('./bot_libs/twitter')
+const {twitterBotHandlerGenerator} = require('./bot_libs/bot')
 require('dotenv').config()
 
 app.use(helmet())
@@ -25,35 +24,11 @@ app.get('/puppeteer', function (request, response) {
 
 // Tweet-making endpoint
 app.get(`/${process.env.tweetEndpoint}`, (request, response) => {
-  const port = process.env.PORT
-  const scheme = `http${port === 443 ? 's' : ''}`
-  const host = `${request.hostname}:${port}`
-  const puppeteerURL = `${scheme}://${host}/puppeteer`
-  const config = {
-    width: 500,
-    height: 200,
-    scalingFactor: 4,
-    wordList: [
-      'trans girls', 'cyber girls', 'cat girls', 'sleepy girls',
-      'nice girls', 'powerful girls', 'sword girls', 'deer girls',
-      'slime girls'
-    ],
-    fontSize: 40
-  }
-  generateRandomGif(
-    config, puppeteerURL,
-    '#textarea-config-string',
-    '#form-gif-config input[type="submit"]',
-    '#gif-link'
-  ).then((result) => {
-    const client = getTwitterClient()
-    sendTweet(client, result.options.text, result.imageData)
-      .then(tweet => console.log(tweet))
-      .catch(error => console.error(error))
-    response.status(200).type(result.imageData.mime).send(result.imageData.buffer)
-  }).catch((err) => {
-    console.error(err)
-    response.status(500).send(err.message)
+  // Load bot configuration
+  fs.readFile('./botconfig.json', 'utf8', (err, data) => {
+    if (err) throw err
+    const botConfig = JSON.parse(data)
+    twitterBotHandlerGenerator(botConfig)(request, response)
   })
 })
 
